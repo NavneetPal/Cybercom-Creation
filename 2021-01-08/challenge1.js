29b67f5996557f67ac389cb3fe662195c005b6b2
Navneet <navneetpal8106@gmail.com>
2021-01-08 19:31:50 +0530

    js directory added

---
+++ 2021-01-08/challenge1.js
@@ -0,0 +1,9 @@
+let markMass=40
+let johnMass=50
+
+let johnHeight=6
+let markHeight=5
+
+let isMarkHigherBmi= markMass/(markHeight**2) >johnMass/(johnHeight**2);
+
+console.log(`Is Marks BMI higher than Johns ? ${isMarkHigherBmi}`)
